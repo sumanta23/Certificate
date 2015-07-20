d113e882cc1f966f8812aeba8475c7c2da9bc6c8
sumanta23 <nandisumanta0@gmail.com>
2015-07-21 00:25:32 +0530

    fixed arquillian

--- src/test/java/org/sumanta/test/it/ITutil.java
+++ src/test/java/org/sumanta/test/it/ITutil.java
@@ -6,45 +6,48 @@ import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class ITutil {
 
-  String responseString = "";
+    String responseString = "";
 
-  public String executeCommand(String baseUrl, String command) {
+    public String executeCommand(String baseUrl, String command) {
 
-    HttpClient client = new HttpClient();
+        HttpClient client = new HttpClient();
 
-    // Create a method instance.
-    GetMethod method = new GetMethod(baseUrl);
+        command = command.replaceAll(" ", "%20");
 
-    // Provide custom retry handler is necessary
-    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
+        // Create a method instance.
+        PostMethod method = new PostMethod(baseUrl + command);
 
-    try {
-      // Execute the method.
-      int statusCode = client.executeMethod(method);
+        // Provide custom retry handler is necessary
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
 
-      if (statusCode != HttpStatus.SC_OK) {
-        System.err.println("Method failed: " + method.getStatusLine());
-      }
+        try {
+            // Execute the method.
+            int statusCode = client.executeMethod(method);
 
-      // Read the response body.
-      byte[] responseBody = method.getResponseBody();
+            if (statusCode != HttpStatus.SC_OK) {
+                System.err.println("Method failed: " + method.getStatusLine());
+            }
 
-      // Deal with the response.
-      // Use caution: ensure correct character encoding and is not binary data
-      responseString = new String(responseBody);
+            // Read the response body.
+            byte[] responseBody = method.getResponseBody();
 
-    } catch (IOException e) {
-      System.err.println("Fatal protocol violation: " + e.getMessage());
-      e.printStackTrace();
-    } finally {
-      // Release the connection.
-      method.releaseConnection();
+            // Deal with the response.
+            // Use caution: ensure correct character encoding and is not binary data
+            responseString = new String(responseBody);
+
+        } catch (IOException e) {
+            System.err.println("Fatal protocol violation: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
+        return responseString;
     }
-    return responseString;
-  }
 
 }
--- src/test/java/org/sumanta/test/it/setup/IntegrationTestDeploymentFactory.java
+++ src/test/java/org/sumanta/test/it/setup/IntegrationTestDeploymentFactory.java
@@ -14,10 +14,8 @@ package org.sumanta.test.it.setup;
 import java.io.File;
 import java.util.Set;
 
-
 import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
-
 
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -27,60 +25,63 @@ import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
 public class IntegrationTestDeploymentFactory {
 
+    public static final File BEANS_XML_FILE = new File("src/test/resources/META-INF/beans.xml");
 
-  public static final File BEANS_XML_FILE = new File("src/test/resources/META-INF/beans.xml");
+    /**
+     * Create deployment from given maven coordinates
+     * 
+     * @param mavenCoordinates
+     *            Maven coordinates in form of groupId:artifactId:type
+     * @return Deployment archive represented by this maven artifact
+     */
+    public static EnterpriseArchive createEARDeploymentFromMavenCoordinates(final String mavenCoordinates) {
+        //log.debug("******Creating deployment {} for test******", mavenCoordinates);
+        final File archiveFile = IntegrationTestDependencies.resolveArtifactWithoutDependencies(mavenCoordinates);
+        System.out.print("maven cordinates " + mavenCoordinates);
+        System.out.print("archive " + archiveFile.getAbsolutePath());
+        if (archiveFile == null) {
+            throw new IllegalStateException("Unable to resolve artifact " + mavenCoordinates);
+        }
+        final EnterpriseArchive ear = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, archiveFile);
 
-  /**
-   * Create deployment from given maven coordinates
-   * 
-   * @param mavenCoordinates
-   *          Maven coordinates in form of groupId:artifactId:type
-   * @return Deployment archive represented by this maven artifact
-   */
-  public static EnterpriseArchive createEARDeploymentFromMavenCoordinates(final String mavenCoordinates) {
-    //log.debug("******Creating deployment {} for test******", mavenCoordinates);
-    final File archiveFile = IntegrationTestDependencies.resolveArtifactWithoutDependencies(mavenCoordinates);
-    if (archiveFile == null) {
-      throw new IllegalStateException("Unable to resolve artifact " + mavenCoordinates);
+        // log.debug("******Created from maven artifact with coordinates {} ******", mavenCoordinates);
+        return ear;
     }
-    final EnterpriseArchive ear = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, archiveFile);
 
-   // log.debug("******Created from maven artifact with coordinates {} ******", mavenCoordinates);
-    return ear;
-  }
+    public static Archive<?> createEarTestDeployment(final Set<Package> testPackagesToAdd) {
+        final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class);
+        final MavenDependencyResolver resolver = getMavenResolver();
+        ear.addAsLibrary(createModuleArchive(testPackagesToAdd));
 
-  public static Archive<?> createEarTestDeployment(final Set<Package> testPackagesToAdd) {
-    final EnterpriseArchive ear = ShrinkWrap.create(EnterpriseArchive.class);
-    final MavenDependencyResolver resolver = getMavenResolver();
-    ear.addAsLibrary(createModuleArchive(testPackagesToAdd));
+        // (2) Libraries needed to perform some tests
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPCOMP).resolveAsFiles());
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPCORE).resolveAsFiles());
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPMIME).resolveAsFiles());
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPCLIENT).resolveAsFiles());
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_COMMONCLIENT).resolveAsFiles());
+        ear.addAsLibraries(resolver.artifact(Dependencies.ORG_JBOSS_SHRINKWRAP_RESOLVER_JAR).resolveAsFiles());
+        // ear.addAsLibraries(resolver.artifact(Dependencies.ORG_JBOSS___RESTEASY).resolveAsFiles());
 
-    // (2) Libraries needed to perform some tests
-    ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPCOMP).resolveAsFiles());
-    ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPCORE).resolveAsFiles());
-    ear.addAsLibraries(resolver.artifact(Dependencies.ORG_APACHE_HTTPMIME).resolveAsFiles());
-    ear.addAsLibraries(resolver.artifact(Dependencies.ORG_JBOSS_SHRINKWRAP_RESOLVER_JAR).resolveAsFiles());
-    ear.addAsLibraries(resolver.artifact(Dependencies.ORG_JBOSS___RESTEASY).resolveAsFiles());
-
-    System.out.println("EAR content:" + ear.toString(true));
-    return ear;
-  }
-
-  /**
-   * This is used to setup the module configuration
-   * 
-   * @return Archive
-   */
-  public static Archive<?> createModuleArchive(final Set<Package> testPackagesToAdd) {
-    final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test-bean-lib.jar");
-    for (final Package p : testPackagesToAdd) {
-      archive.addPackage(p);
+        System.out.println("EAR content:" + ear.toString(true));
+        return ear;
     }
-    archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    return archive;
-  }
 
-  public static MavenDependencyResolver getMavenResolver() {
-    return DependencyResolvers.use(MavenDependencyResolver.class).loadMetadataFromPom("pom.xml");
-  }
+    /**
+     * This is used to setup the module configuration
+     * 
+     * @return Archive
+     */
+    public static Archive<?> createModuleArchive(final Set<Package> testPackagesToAdd) {
+        final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, "test-bean-lib.jar");
+        for (final Package p : testPackagesToAdd) {
+            archive.addPackage(p);
+        }
+        archive.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return archive;
+    }
+
+    public static MavenDependencyResolver getMavenResolver() {
+        return DependencyResolvers.use(MavenDependencyResolver.class).loadMetadataFromPom("pom.xml");
+    }
 
 }
